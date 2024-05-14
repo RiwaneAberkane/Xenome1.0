3a5bd4a48a24c86ae70d5cb119ad8fe4b69de512
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2024-05-14 18:04:07 +0200

    wip navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="nav-left">
+                <h1>Xenome</h1>
+            </div>
+            <div className="nav-right">
+                <ul>
+                    <li>Accueil</li>
+                    <li>Articles</li>
+                    <li>Contact</li>
+                    <li>À propos</li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
