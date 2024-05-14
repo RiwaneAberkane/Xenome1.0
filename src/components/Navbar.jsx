97b8cd41e3d6856a85427e8ad4eedb76feb4f579
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2024-05-14 21:15:55 +0200

    wip...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import "./Navbar.css"
+import hamburger from "../assets/ham-menu.svg"
+import close from "../assets/ham-menu-close.svg"
 
 
 const Navbar = () => {
@@ -8,13 +10,22 @@ const Navbar = () => {
                 <h1>Xenome</h1>
             </div>
             <div className="nav-right">
-                <ul>
+                <ul className="header_link">
                     <li><a href="">Accueil</a></li>
                     <li><a href="">Articles</a></li>
                     <li><a href="">À propos</a></li>
                     <li><a href="">Contact</a></li>
                 </ul>
             </div>
+
+            <div className="header__main-ham-menu-cont">
+                <img id="menu-icon" src={hamburger} alt="hamburger menu" className="header__main-ham-menu" />
+                <img id="close-icon" src={close} alt="hamburger menu close" className="header__main-ham-menu-close d-none" />
+
+
+            </div>
+
+
         </nav>
     );
 }
